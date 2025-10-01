1fa3a5a171a3be1fd1de27206f37b937cd700fd0
Junkrat <kamrakocz@gmail.com>
2025-10-01 16:04:27 +0200

    Dodaj pliki projektów.

---
+++ LabConsoleApp-01/LabConsoleApp-01.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello GitHub!" << endl;
+    int holyWH40kNumber = 42137;
+    cout << "Venerate the Immortal Emperor: " << holyWH40kNumber << endl;
+}
