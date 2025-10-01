81c08a50ae693384da8d7ff02be8fbc4a870d39e
Junkrat <kamrakocz@gmail.com>
2025-10-01 16:59:03 +0200

    Nowsza wersja projektu v.0.1b

--- LabConsoleApp-01/LabConsoleApp-01.cpp
+++ LabConsoleApp-01/LabConsoleApp-01.cpp
@@ -6,4 +6,9 @@ int main()
     cout << "Hello GitHub!" << endl;
     int holyWH40kNumber = 42137;
     cout << "Venerate the Immortal Emperor: " << holyWH40kNumber << endl;
+
+    double zmienna = 0;
+    cout << "Podaj liczbe wymierna:";
+    cin >> zmienna;
+    cout << "Wprowadzono: " << zmienna;
 }
